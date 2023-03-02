0043a560ab1060e440e17efda9b8107bc33f5296
Matt Peterson <mattkpeterson94@gmail.com>
2023-03-01 22:52:23 -0600

    2nd commit

--- src/Test.java
+++ src/Test.java
@@ -8,7 +8,7 @@ public class Test {
 		if (hungry == "yes")
 			System.out.println("eat something....");
 		else {
-			System.out.println("Do work...");
+			System.out.println("Do work.....");
 		}
 	}
 	
